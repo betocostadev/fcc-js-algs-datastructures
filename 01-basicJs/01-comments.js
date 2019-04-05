b218437618d3c605908d9fb0e2f0e64432f26545
Beto Costa <robertomarx@gmail.com>
2019-04-05 10:06:15 -0300

    started

---
+++ 01-basicJs/01-comments.js
@@ -0,0 +1,8 @@
+// This is a single line comment.
+/* This is a multi line comment
+as you can see */
+let bestPractice = `Exercise 1:
+Best Practice
+As you write code, you should regularly add comments to clarify the function of parts of your code.
+Good commenting can help communicate the intent of your code—both for others and for your future self.`
+console.log(bestPractice);
