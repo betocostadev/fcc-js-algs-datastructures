a042278fb1b39353c60306b678c7c11b285c812a
Beto Costa <robertomarx@gmail.com>
2019-04-24 11:03:08 -0300

    added loops

--- 01-basicJs/16-loops.js
+++ 01-basicJs/16-loops.js
@@ -62,3 +62,69 @@ for(let i = 0; i < craps.length; i++) {
   total += craps[i];
 }
 console.log(total);
+
+/* NESTING LOOPS */
+/* If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. Here is an example:
+
+var arr = [
+  [1,2], [3,4], [5,6]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+This outputs each sub-element in arr one at a time. Note that for the inner loop, we are checking the .length of arr[i], since arr[i] is itself an array. */
+console.log('\nNesting loops:\n');
+let multiArr = [[1,2], [3,4], [5,6]];
+console.log(multiArr);
+// Logging it with only one loop
+console.log('\nUsing only one loop:');
+for (let i = 0; i < multiArr.length; i++) {
+    console.log(multiArr[i]);
+}
+// Using the function below to execute a nested loop and display all the items.
+function nestedCheck(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++)
+        console.log(arr[i][j]);
+    }
+}
+console.log('\nChecking all the elements of the nested arrays and displaying then:')
+nestedCheck(multiArr);
+
+/* Nested loop example on FCC */
+function multiplyAll(arr) {
+    var product = 1;
+    // Only change code below this line
+    for(var i = 0; i < arr.length; i++) {
+      for(var j = 0; j < arr[i].length; j++) {
+        // console.log(arr[i][j]);
+        product *= arr[i][j];
+      }
+    }
+// Only change code above this line
+    return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
+
+/* ITERATE WITH DO...WHILE LOOPS */
+/* The next type of loop you will learn is called a "do...while" loop because it first will "do" one pass of the code inside the loop no matter what, and then it runs "while" a specified condition is true and stops once that condition is no longer true. Let's look at an example. */
+
+/* The main difference of the Do...while loop is that the first 'pass' will always run, so
+it will execute the code block inside the loop for the first time, no matter what, then, if
+the conditions are not match on the second iteration, it will stop. */
+console.log('\nDo While Loop:\n');
+
+var whileArr1 = [];
+var adding = 5;
+do {
+    whileArr1.push(adding);
+    adding++;
+} while (adding < 5);
+// It should not work since the adding is already + than 5, but it will run the first time.
+console.log(whileArr1);
+/* Essentially, a do...while loop ensures that the code inside the loop will run at least once. */
