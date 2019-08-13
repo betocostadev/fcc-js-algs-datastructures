7e83376682b77ee4ec41c7a95876f4356c3964c3
betocostadev <robertomarx@gmail.com>
2019-08-13 15:54:54 -0300

    added more to regex test

--- 03-regex/01-test-match.js
+++ 03-regex/01-test-match.js
@@ -2,7 +2,8 @@
 Regular Expressions
 */
 
-/* REGEX Using the .test() method
+/*
+REGEX Using the .test() method
 */
 
 console.log(`
@@ -11,13 +12,29 @@ Using the .test() method:
 `);
 
 const string1 = 'freeCodeCamp';
-let myregex1 = /Code/;
-const test1 = myregex1.test(string1); // case sensitive!
+const myRegex1 = /Code/;
+const test1 = myRegex1.test(string1); // case sensitive!
+console.log(`Does ${string1} contains ${myRegex1}?`)
 console.log(test1);
+
+console.log(`Getting the position of the found Regex in a string: `)
+if (myRegex1) {
+  const regex1String = String(myRegex1).substring(1, 5);
+  // console.log(regex1String)
+  console.log(string1.indexOf(regex1String))
+}
 
 console.log(`
 === Match a Literal String with Different Possibilities ===`)
+
 const string2 = 'Does the cat eats birds?'
-let myregex2 = /dog|bird/
-const test2 = myregex2.test(string2);
-console.log(test2);
+const myRegex2 = /dog|bird/
+const test2 = myRegex2.test(string2)
+console.log(test2)
+
+console.log(`
+=== Ignore case while matching ===`)
+const string3 = 'This is UsiNG lowER and UPPer case'
+const myRegex3 = /using/i
+const test3 = myRegex3.test(string3)
+console.log(`Does ${myRegex3} matches: ${string3}? ${test3}`)
