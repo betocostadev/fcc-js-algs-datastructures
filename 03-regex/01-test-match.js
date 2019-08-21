8c53dd85ed1006685d1fa211e491512491bf696c
betocostadev <robertomarx@gmail.com>
2019-08-21 17:00:30 -0300

    completed regex intro

--- 03-regex/01-test-match.js
+++ 03-regex/01-test-match.js
@@ -14,10 +14,10 @@ REGEX - Index
 109 - Match Letters of the Alphabet
 116 - Match Numbers and Letters of the Alphabet
 124 - Match Single Characters Not Specified (negate characters)
-
-
-
-
+133 - Match Characters that Occur One or More Times
+148 - Match Characters that Occur Zero or More Times
+169 - Find Characters with Lazy Matching
+184 - Challenge: Find One or More Criminals in a Hunt
 
 */
 
@@ -129,3 +129,65 @@ const quote3Sample = "3 42 18 Godzilla killed the blind mice."
 const negator1 = /[^aeiou0-9 ]/gi // notice the space, also negated
 const result4 = quote3Sample.match(negator1)
 console.log(result4)
+
+// Match Characters that Occur One or More Times
+console.log(`
+=== Match Characters that Occur One or More Times ===`)
+const quote4Sample = 'More than one \'a\' in Aadvark'
+console.log(quote4Sample)
+const quote5Sample = 'Characters not in a row, xbxb'
+const repeatingAs = /a+/gi
+const repeatXs = /x+/gi
+const result5 = quote4Sample.match(repeatingAs)
+const result6 = quote5Sample.match(repeatXs)
+console.log(result5)
+console.log('For the x, will find 2, not in a row because the /x+/ will look only for the x')
+console.log(quote5Sample)
+console.log(result6)
+
+// Match Characters that Occur Zero or More Times
+console.log(`
+=== Match Characters that Occur Zero or More Times ===`)
+const chewieQuote = 'Aaaaaaaarrrgh!'
+const chewieRegex = /Aa*/gi
+const resultChewie = chewieQuote.match(chewieRegex)
+console.log(resultChewie)
+console.log(`To recap:
+char+ = One or more characters before the +
+char* = Zero or more characters before the *`)
+
+const someGPhrase = 'goooooal!'
+const someOPhrase = 'gut feeling'
+const zeroOrMoreRegex = /go*/
+console.log(someGPhrase)
+console.log(someGPhrase.match(zeroOrMoreRegex))
+console.log(someOPhrase)
+console.log(someOPhrase.match(zeroOrMoreRegex))
+
+// Find Characters with Lazy Matching
+console.log(`
+=== Find Characters with Lazy Matching ===`)
+console.log(`In regular expressions, a greedy match finds the longest possible part of a string that fits the regex pattern and returns it as a match. The alternative is called a lazy match, which finds the smallest possible part of the string that satisfies the regex pattern.`)
+const textHTML = '<h1>Winter is coming</h1>'
+const regexHTMLTag = /<*h1>/g
+const resultHTMLTagRegex = textHTML.match(regexHTMLTag)
+console.log(resultHTMLTagRegex)
+
+const titanic = 'titanic'
+const titanicRegex = /t[a-z]*i/
+const lazyTitanicRegex = /t[a-z]*?i/
+const resultLazyTitanic = titanic.match(lazyTitanicRegex)
+const resultTitanic = titanic.match(titanicRegex)
+console.log(resultTitanic)
+console.log(resultLazyTitanic)
+
+// Challenge: Find One or More Criminals in a Hunt
+console.log(`
+=== Challenge: Find One or More Criminals in a Hunt ===`)
+// example crowd gathering
+const crowd = 'P1CP2CCP3CCCP4P5CCCCP6P7P8CCCCCCCP9P10P11';
+
+const reCriminals = /c+/gi; // Change this line
+
+const matchedCriminals = crowd.match(reCriminals);
+console.log(matchedCriminals);
